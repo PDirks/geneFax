3f050da430fa455dcc3a0c2a3d5953df96739063
ChaseGHMU <cjan8c@mail.missouri.edu>
2016-10-15 00:07:31 -0500

    Test Commit

--- src/genefax/DBmanager.java
+++ src/genefax/DBmanager.java
@@ -6,12 +6,12 @@
 package genefax;
 
 import java.sql.*;
-
+import java.util.ArrayList;
 /**
  *
  * @author pete
  */
-public class DBmanager {
+public class DBmanager extends GeneFax {
     
     DBmanager(){}
     
@@ -19,7 +19,7 @@ public class DBmanager {
         Connection c = null;
         try {
           Class.forName("org.sqlite.JDBC");
-          c = DriverManager.getConnection("jdbc:sqlite:test.db");
+          c = DriverManager.getConnection("jdbc:sqlite:GeneFax.db");
         } catch ( Exception e ) {
           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
           System.exit(0);
@@ -27,4 +27,85 @@ public class DBmanager {
         return "Opened database successfully";
     }
     
+    public String createTable(){
+        Connection c = null;
+        Statement stmt = null;
+        Statement stmt2 = null;
+        Statement stmt3 = null;
+        
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:GeneFax.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String genes = "CREATE TABLE GENES" +
+                           "(GENE_ID INT PRIMARY KEY" +
+                           "GENE_NAME TEXT NOT NULL" + 
+                           "DATA_LABEL TEXT" + 
+                           "DATA_AVG FLOAT)";
+             
+            stmt.executeUpdate(genes);
+            stmt.close();
+             
+            stmt2 = c.createStatement();
+            String geneID = "CREATE TABLE ID" +
+                            "(DATA_POINTS FLOAT" +
+                            "GENE_ID PRIMARY KEY REFERENCES GENES(GENE_ID)";
+            stmt2.executeUpdate(geneID);
+            stmt2.close();
+             
+            stmt3 = c.createStatement();
+            String geneFold = "CREATE TABLE FOLD" +
+                              "(FOLDS FLOAT" +
+                              "GENE_ID PRIMARY KEY REFERENCES GENES(GENE_ID)";
+            
+            stmt3.executeUpdate(geneFold);
+            stmt3.close();
+             
+            c.close();
+             
+            } catch ( Exception e ) {
+             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+             System.exit(0);
+            };
+        return "Table created successfully";
+    }
+    
+    public String insert(){
+        ImportManager csvData = new ImportManager();
+        ArrayList<GeneDataRow> data = new ArrayList<GeneDataRow>(100);
+        
+        data = csvData.importCSV();
+        Connection c = null;
+        Statement stmt = null;
+        int i;
+        String sql;
+        
+        try {
+          Class.forName("org.sqlite.JDBC");
+          c = DriverManager.getConnection("jdbc:sqlite:GeneFax.db");
+          c.setAutoCommit(false);
+          System.out.println("Opened database successfully");
+
+          for(i = 0; i < data.size(); i++){
+          stmt = c.createStatement();
+          sql = "INSERT INTO GENES (GENE_ID,GENE_NAME)" +
+                       "VALUES("+ data<1> + ; 
+          stmt.executeUpdate(sql);
+          }
+          
+          stmt.close();
+          c.commit();
+          c.close();
+        } catch ( Exception e ) {
+          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+          System.exit(0);
+        }
+
+        csvData.importCSV();
+        
+        return "Inserted Correctly";
+    }
 }
+
